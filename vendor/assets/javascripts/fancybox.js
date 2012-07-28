609e36f3754dabf11a784beee5e3b4ebb91d8a5e
Jon Stacey <jon@jonsview.com>
2012-07-27 21:05:07 -0600

    Added initializer so that everything works out of the box via HTML5 data attributes.

--- vendor/assets/javascripts/fancybox.js
+++ vendor/assets/javascripts/fancybox.js
@@ -2,3 +2,7 @@
 //= require jquery.fancybox-buttons
 //= require jquery.fancybox-thumbs
 //= require jquery.fancybox-media
+
+$(document).ready(function() {
+  $('a[data-fancybox="true"]').fancybox();
+});
